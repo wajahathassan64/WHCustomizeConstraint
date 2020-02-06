165ecb66a5f5e21aa2b00d098e78b43ff7d2d97a
Wajahat Hassan <wajahat@digitify.com>
2020-02-06 11:24:57 +0500

    Adding Carthage Support

--- Example/WHCustomizeConstraint/ViewController.swift
+++ Example/WHCustomizeConstraint/ViewController.swift
@@ -34,7 +34,6 @@ class ViewController: UIViewController {
     }()
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
